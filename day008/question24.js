0caabca7b78ebd3023398658387772fd1a10beb3
ZAHEER ABBAS <zaheerszhrs@gmail.com>
2024-03-10 12:20:10 +0500

    add files

---
+++ day008/question24.js
@@ -0,0 +1,21 @@
+/*
+Question 24: More Conditional Tests: Expand your conditional tests to cover
+a wider range of comparisons.
+*/
+// conditions with strings
+console.log("\nconditions with strings");
+console.log("car == Car ", "\"False\"");
+console.log("boy == boy ", "\"True\"");
+// string conditions using lowercase function
+console.log("\nconditions with strings");
+console.log("Car".toLowerCase() == "car", "\"True\"");
+// Numarical conditions
+console.log("\nNumarical conditons test");
+console.log("5 == 6", "\"False\"");
+console.log("6 == 6", "\"True\"");
+console.log("2 > 6", "\"False\"");
+// Test whether an item is in a array and whether an item is not in a array
+console.log("\nTest whether an item is in a array and whether an item is not in a array");
+var company = ["Hp", "lenovo"];
+console.log(company.includes("Hp"), "\"True\""); // Is hp in company
+console.log(company.includes("dell"), "\"False\""); // Is dell in company
